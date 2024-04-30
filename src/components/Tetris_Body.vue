a0620673e6b114511703c655c300a7f8d788ce9b
GiuseppeF11 <faillagiuseppe13@gmail.com>
2024-04-30 18:22:35 +0200

    Add grid for blocks

--- src/components/Tetris_Body.vue
+++ src/components/Tetris_Body.vue
@@ -10,17 +10,64 @@ export default {
 
     }
 }
+
+/* TETRIS */
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('ciao'); //test
+    const screen = document.getElementById('screen');       //Selezioniamo il campo di gioco
+    const ctx = screen.getContext('2d');       //Consente di disegnare sullo screen , il canvas in questo caso
+
+    //SCHERMO
+    const SCALE_F = 20;
+    const COLOR1 = '#00AA8D';
+    const COLOR2 = '#00BF9A';
+
+    //GRIGLIA
+    ctx.scale(SCALE_F,SCALE_F)
+    for (let i = 0; i < screen.width / SCALE_F; i++) {
+        ctx.fillStyle = i % 2 === 0 ? COLOR1 : COLOR2;
+        ctx.fillRect( i , 0 , 1 , screen.height / SCALE_F );
+        console.log(i);
+    }
+
+    //BLOCCHI
+    let element =  [
+        [ 0 , 1 , 0 , 0],
+        [ 1 , 1 , 1 , 0],
+        [ 0 , 0 , 0 , 0],
+        [ 0 , 0 , 0 , 0],
+    ]
+
+    ctx.fillStyle = 'green';
+
+    function drawElement(el) {
+        element.forEach((row, y) => {
+            row.forEach((v , x) => {
+                console.log(v , x , y);
+                if (v != 0) {
+                    ctx.fillRect(x , y , 1 , 1);
+                }
+            })
+        })
+    }
+
+    drawElement(element);
+});
 </script>
 
 <template>
-    <div class="tetris-container m-auto bg-danger">
+    <canvas id="screen" class="screen m-auto border-4 ">
         ciao
-    </div>
+    </canvas>
 </template>
+ 
+
 
 <style lang="scss" scoped>
-.tetris-container {
-    width: 30vw;
-    height: 70vh;
+.screen {
+    width: 400px;
+    height: 400px;
 }
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,18 +3,5 @@ import App from './App.vue'
 import * as bootstrap from 'bootstrap'
 
 
-/* import { library } from '@fortawesome/fontawesome-svg-core'
-
-
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-
-
-library.add(faUserSecret) */
-
-
 createApp(App)
-/* .component('font-awesome-icon', FontAwesomeIcon) */
 .mount('#app')
